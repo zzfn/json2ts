bb2d53798110bf274ecad07815a8bfdf45b7ab12
cw <44751363+zzfn@users.noreply.github.com>
2023-08-19 22:38:31 +0800

    feat: init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 export default {
   output: 'standalone',
+  assetPrefix:
+      process.env.NODE_ENV === 'production' && !process.env.VERCEL
+          ? 'https://cdn.zzfzzf.com/json2ts'
+          : '/',
   reactStrictMode: true,
 };
